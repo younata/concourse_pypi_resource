7217adfc64502a6ca747502403c1fe36e1969351
Rachel Brindle <you@subluminal.net>
2018-01-24 21:15:13 -0800

    Returns values as list of 'version':  objects

--- lib/check.py
+++ lib/check.py
@@ -1,15 +1,33 @@
 import json
+import sys
 from urllib.request import urlopen
 from distutils.version import LooseVersion
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 
-def check(package_name: str, existing_version: Optional[str]) -> List[str]:
+def check(package_name: str, existing_version: Optional[Dict[str, str]]) -> List[Dict[str, str]]:
     url = "https://pypi.python.org/pypi/%s/json" % package_name
     data = json.load(urlopen(url))
-    versions = list(data["releases"].keys())
-    versions.sort(key=LooseVersion, reverse=True)
-    if existing_version is not None:
-        return [v for v in versions if LooseVersion(v) >= LooseVersion(existing_version)]
+    found_versions = list(data["releases"].keys())
+    found_versions.sort(key=LooseVersion, reverse=True)
+
+    if existing_version is not None and \
+                    existing_version.get("version") is not None:
+        existing = LooseVersion(existing_version["version"])
+        newer_versions = [v for v in found_versions if LooseVersion(v) >= existing]
     else:
-        return versions
+        newer_versions = [found_versions[0]]
+
+    return _wrap(newer_versions)
+
+
+def main():
+    source = json.load(sys.stdin)
+    print(json.dumps(check(
+        package_name=source['source']['package'],
+        existing_version=source.get('version')
+    )))
+
+
+def _wrap(versions: List[str]) -> List[Dict[str, str]]:
+    return [{"version": v} for v in versions]
--- tests/test_check.py
+++ tests/test_check.py
@@ -1,12 +1,12 @@
 import json
 from unittest import TestCase, mock
 
-from lib.check import check
+from lib.check import check, main
 
 
+@mock.patch("lib.check.urlopen")
 class TestCheck(TestCase):
-    @mock.patch("lib.check.urlopen")
-    def test_returns_all_versions_in_pypi_if_existing_version_unspecified(self, mock_urlopen):
+    def test_returns_last_version_in_pypi_if_existing_version_unspecified(self, mock_urlopen):
         mock_urlopen.return_value.read.return_value = json.dumps(
             {"releases": {
                 "1.0": {},
@@ -19,17 +19,13 @@ class TestCheck(TestCase):
         self.assertEqual(
             check("foo", None),
             [
-                "3.0",
-                "2.1rc1",
-                "1.2",
-                "1.0",
+                {"version": "3.0"}
             ]
         )
 
         mock_urlopen.assert_called_once_with("https://pypi.python.org/pypi/foo/json")
 
-    @mock.patch("lib.check.urlopen")
-    def test_returns_all_versions_in_pypi_if_existing_version_specified(self, mock_urlopen):
+    def test_returns_versions_in_pypi_more_recent_and_equal_to_existing_if_existing_version_specified(self, mock_urlopen):
         mock_urlopen.return_value.read.return_value = json.dumps(
             {"releases": {
                 "1.0": {},
@@ -40,9 +36,49 @@ class TestCheck(TestCase):
         )
 
         self.assertEqual(
-            check("foo", "2.1rc1"),
+            check("foo", {"version": "2.1rc1"}),
             [
-                "3.0",
-                "2.1rc1"
+                {"version": "3.0"},
+                {"version": "2.1rc1"}
             ]
         )
+
+
+@mock.patch("lib.check.print")
+@mock.patch("lib.check.check", return_value=[])
+@mock.patch("lib.check.sys.stdin")
+class TestMain(TestCase):
+    def test_when_only_source_is_given_it_parses_stdin_as_json_and_passes_source_to_check(self, mock_stdin, mock_check, _):
+        mock_stdin.read.return_value = '{"source": {"package": "tox"}}'
+
+        main()
+
+        mock_check.assert_called_once_with(package_name="tox", existing_version=None)
+
+    def test_when_both_source_and_version_is_given_it_parses_stdin_as_json_and_passes_both_to_check(self, mock_stdin, mock_check, _):
+        mock_stdin.read.return_value = json.dumps({
+            "source": {"package": "tox"},
+            "version": {"version": "3.0"}
+        })
+
+        main()
+
+        mock_check.assert_called_once_with(package_name="tox", existing_version={"version": "3.0"})
+
+    def test_when_version_is_given_but_is_none_it_parses_stdin_as_json_and_passes_version_as_none_to_check(self, mock_stdin, mock_check, _):
+        mock_stdin.read.return_value = json.dumps({
+            "source": {"package": "tox"},
+            "version": None
+        })
+
+        main()
+
+        mock_check.assert_called_once_with(package_name="tox", existing_version=None)
+
+    def test_when_check_finishes_prints_result_as_json_to_stdout(self, mock_stdin, mock_check, mock_print):
+        mock_stdin.read.return_value = '{"source": {"package": "tox"}}'
+        mock_check.return_value = [{"version": "1.0"}]
+
+        main()
+
+        mock_print.assert_called_once_with('[{"version": "1.0"}]')
